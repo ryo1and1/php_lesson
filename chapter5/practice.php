49a48aac4cfa75ef9ba8f2e3c845b009d3cef518
ryo1and1 <nyaho8056@gmail.com>
2019-01-28 21:37:54 +0900

    add chapter5

---
+++ chapter5/practice.php
@@ -0,0 +1,22 @@
+<?php
+$message = "Hello World" ;
+var_dump($message);
+?>
+
+<?php
+$number = 1234567890 ;
+var_dump($number);
+?>
+
+<?php
+if ($fruit == "apple"){
+	echo "‰Ê•¨‚ÍƒŠƒ“ƒS‚Å‚µ‚½";
+}
+var_dump($fruit);
+?>
+
+<?php
+$fruits = array("apple","orange","grape");
+var_dump($fruits)
+?>
+
