a92634edac89c988cd709f1ed161e0484fa1c213
ryo1and1 <nyaho8056@gmail.com>
2019-01-29 19:50:12 +0900

    add chapter7

---
+++ chapter7/practice.php
@@ -0,0 +1,34 @@
+<?php
+$result = 3+5;
+echo $result;
+?>
+
+<?php
+$result2 = 7 - 4;
+echo $result2;
+?>
+
+<?php
+$result3 = 3 * 7;
+echo $result;
+?>
+
+<?php
+$result4 = 8 / 4;
+echo $result4;
+?>
+
+<?php
+$result5 = 7 % 4;
+echo $result5;
+?>
+
+<?php
+$result6 = 10 + 5 * 6 / 2 - 2;
+echo $result6;
+?>
+
+<?php
+$result7 = 2 ** 11;
+echo $result7;
+?>
