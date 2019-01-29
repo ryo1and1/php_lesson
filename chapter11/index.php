da2e1fca6367fca0a61d94a95dac59a826cc949a
ryo1and1 <nyaho8056@gmail.com>
2019-01-29 23:35:46 +0900

    add tenki

--- chapter11/index.php
+++ chapter11/index.php
@@ -1,6 +1,6 @@
 <?php
 // 時間帯(朝 or 昼 or 夜)
-$time_zone = "eveningg";
+$time_zone = "evening";
 //時間帯ごとに挨拶を変えて表示する
 switch ($time_zone){
 case("morning"):
@@ -15,6 +15,26 @@ case("night"):
 	break;
 default:
 	echo "おっす" .PHP_EOL;
+	break;
+}
+?>
+
+<?php
+//天気(晴or曇りor雨)
+$tenki = "sunny";
+//天気によって行動を変える
+switch($tenki){
+case("sunny"):
+	echo "釣り" .PHP_EOL;
+	break;
+case("cloudy"):
+	echo "走る" .PHP_EOL;
+	break;
+case("rainy"):
+	echo "読書" .PHP_EOL;
+	break;
+default:
+	echo "家事" .PHP_EOL;
 	break;
 }
 ?>
